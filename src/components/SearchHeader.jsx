0ab98e588f926dc6060d62dcbae7e2a2332e2cdf
hyelight <zheldeld@gmail.com>
2023-02-16 16:35:01 +0900

    헤더 스타일링

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -14,19 +14,20 @@ const SearchHeader = () => {
   useEffect(() => setText(keyword || ""), [keyword]);
 
   return (
-    <header>
-      <Link to="/">
-        <BsYoutube />
-        <h1>Metube</h1>
+    <header className="flex border-b border-gray-600 text-2xl p-3.5 ">
+      <Link to="/" className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold text-3xl ml-3">Metube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="w-full flex justify-center">
         <input
+          className="w-8/12 p-1 bg-black text-#9CA39F-400 text-xl rounded-3xl p-1.5 pl-5"
           type="text"
           placeholder="검색"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button>
+        <button className="ml-4">
           <BsSearch />
         </button>
       </form>
