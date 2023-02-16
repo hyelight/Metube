c706dfa76f0bb5d6c956d641eb65dbdcd8d7253f
hyelight <zheldeld@gmail.com>
2023-02-16 18:37:13 +0900

    axios로 바꿈

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
 import VideoLi from "../components/VideoLi";
@@ -10,9 +11,9 @@ const Videos = () => {
     error,
     data: videos,
   } = useQuery(["videos", keyword], async () => {
-    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+    return axios
+      .get(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.data.items);
   });
   return (
     <>
