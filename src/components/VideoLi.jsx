f37fd2b5db0e347ef2fe9bd7bee4f81a577314b5
hyelight <zheldeld@gmail.com>
2023-02-16 21:55:40 +0900

    시간 흐름 표시 컴포넌트 추가

--- src/components/VideoLi.jsx
+++ src/components/VideoLi.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import TimeAgo from "./TimeAgo";
 
 const VideoLi = ({ video }) => {
-  return <div>{video.snippet.description}</div>;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <p>{channelTitle}</p>
+      <p>{publishedAt}</p>
+      <p>{TimeAgo(new Date(publishedAt))}</p>
+    </li>
+  );
 };
 
 export default VideoLi;
